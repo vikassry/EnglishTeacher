b3802a28e7dd4ab25be08c4a02841930051a96d7
vikas <vikassry@gmail.com>
2015-09-27 17:36:50 +0530

    cool faces added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var cool = require('cool-ascii-faces');
 
 /* GET home page. */
+
+router.get('/cool', function(req, res){
+	res.send(cool());
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
