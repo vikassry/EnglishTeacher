af85d95c20bacca2fadcf651041acef8b2c16084
vikas <vikassry@gmail.com>
2015-09-27 18:01:14 +0530

    postgresql added

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,21 @@ router.get('/cool', function(req, res){
 	res.send(cool());
 });
 
+
+var pg = require('pg');
+
+router.get('/db', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM test_table', function(err, result) {
+			done();
+			if (err)
+				{ console.error(err); response.send("Error " + err); }
+			else
+				{ response.send({results: result.rows}); }
+		});
+	});
+})
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
